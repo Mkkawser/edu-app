a303da0b963855b0bc03fdc5e890d3d8ddb2627b
Kawser Hossain <irumorgan420@gmail.com>
2023-02-18 09:43:22 +0600

    heroBttom

--- component/hero/heroBottom.js
+++ component/hero/heroBottom.js
@@ -4,73 +4,44 @@ const HeroBottom = () => {
       <div className="p-4 lg:mx-auto lg:w-[900px]">
         <div className="bg-[#4D2C5E] rounded-[20px] p-1 md:flex md:p-2 md:gap-4 lg:p-4">
           {/*  3 Accebility  */}
-          <div className="flex items-center gap-2 p-2 pr-[2.5vw] md:pr-0 md:p-1">
-            {/* Card Image */}
-            <div className="bg-[rgba(255,255,255,0.2)] p-2 max-w-[20%] rounded-[20px]">
-              <img
-                className="w-full object-cover"
-                src="/img/hero-online-test.png"
-                alt="online-test"
-              />
-            </div>
 
-            {/* Card Text */}
-            <div className=" max-w-[60vw] font-Roboto text-white">
-              <p className="text-[3.5vw] md:text-[18px] md:w-max">
-                Learn The Latest Skills
-              </p>
-              <p className="text-[rgba(255,255,255,0.6)] text-[2.7vw] md:text-[11px]">
-                Contrary to popular belief, Lorem Ipsum is not simply random
-                text. It has roots in a BC, making it over 2000 years old.
-              </p>
-            </div>
-          </div>
+          <HeroCard
+            imageSrc="/img/hero-online-test.png"
+            title="Learn The Latest Skills"
+            description="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a BC, making it over 2000 years old."
+          />
 
-          <div className="flex items-center gap-2 p-2 pr-[2.5vw] md:pr-0 md:p-1">
-            {/* Card Image */}
-            <div className=" bg-[rgba(255,255,255,0.2)] p-2 max-w-[20%] rounded-[20px]">
-              <img
-                className="w-full object-cover"
-                src="/img/hero-exam.png"
-                alt="exam"
-              />
-            </div>
-            {/* Card Text */}
-            <div className=" max-w-[60vw] font-Roboto text-white">
-              <p className="text-[3.5vw] md:text-[18px] md:w-max">
-                Get Ready For a Career
-              </p>
-              <p className="text-[rgba(255,255,255,0.6)] text-[2.7vw] md:text-[11px]">
-                Contrary to popular belief, Lorem Ipsum is not simply random
-                text. It has roots in a BC, making it over 2000 years old.
-              </p>
-            </div>
-          </div>
+          <HeroCard
+            imageSrc="/img/hero-exam.png"
+            title="Get Ready For a Career"
+            description="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a BC, making it over 2000 years old."
+          />
 
-          <div className="flex items-center gap-2 p-2 pr-[2.5vw] md:pr-0 md:p-1">
-            {/* Card Image */}
-            <div className=" bg-[rgba(255,255,255,0.2)] p-2 max-w-[20%] rounded-[20px]">
-              <img
-                className="w-full object-cover"
-                src="/img/hero-certification.png"
-                alt="certification"
-              />
-            </div>
-
-            {/* Card Text */}
-            <div className=" max-w-[60vw] font-Roboto text-white">
-              <p className="text-[3.5vw] md:text-[18px] md:w-max">
-                Earn a Certificate
-              </p>
-              <p className="text-[rgba(255,255,255,0.6)] text-[2.7vw] md:text-[11px]">
-                Contrary to popular belief, Lorem Ipsum is not simply random
-                text. It has roots in a BC, making it over 2000 years old.
-              </p>
-            </div>
-          </div>
+          <HeroCard
+            imageSrc="/img/hero-certification.png"
+            title="Earn a Certificate"
+            description="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a BC, making it over 2000 years old."
+          />
         </div>
       </div>
     </>
+  );
+};
+
+const HeroCard = ({ imageSrc, title, description }) => {
+  return (
+    <div className="flex items-center gap-2 p-2 pr-[2.5vw] md:pr-0 md:p-1">
+      <div className="bg-[rgba(255,255,255,0.2)] p-2 max-w-[20%] rounded-[20px]">
+        <img className="w-full object-cover" src={imageSrc} alt={title} />
+      </div>
+
+      <div className="max-w-[60vw] font-Roboto text-white">
+        <p className="text-[3.5vw] md:text-[18px] md:w-max">{title}</p>
+        <p className="text-[rgba(255,255,255,0.6)] text-[2.7vw] md:text-[11px]">
+          {description}
+        </p>
+      </div>
+    </div>
   );
 };
 
